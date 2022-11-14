af3bb02e474ae62349875675c8060d7a79b5df0b
doyeoo <ehdusla013@gmail.com>
2022-11-14 17:41:39 +0900

    feat: 식당 정보 업로드

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import sys
+application = Flask(__name__)
+
+@application.route("/") 
+def home():
+    return render_template("index.html")
+@application.route("/list") 
+def list():
+    return render_template("list.html")
+@application.route("/register_restaurant") 
+def register_restaurant():
+    return render_template("register_restaurant.html")
+@application.route("/register_menu") 
+def register_menu():
+    return render_template("register_menu.html")
+@application.route("/register_review") 
+def register_review():
+    return render_template("register_review.html")
+
+
+@application.route("/submit_restaurant_post", methods=['POST']) 
+def submit_restaurant_post():
+    if request.files["file"] :
+        print('abc')
+        image_file=request.files["file"] 
+        image_path = "static/image/{}".format(image_file.filename)
+        image_file.save(image_path) 
+        print(image_path)
+    data=request.form
+    return render_template("result.html", data=data, path=image_path)
+
+@application.route("/submit_menu_post", methods=['POST']) 
+def submit_menu_post():
+    data=request.form
+    return render_template("index.html", data=data)
+
+@application.route("/submit_review_post", methods=['POST']) 
+def submit_review_post():
+    data=request.form
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__": application.run(host='0.0.0.0')
