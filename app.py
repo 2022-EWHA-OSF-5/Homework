14f9314f3e34fff7e6d906b6706ebdf2061c8aba
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-11-22 17:29:22 +0900

    feat : week12

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect,url_for
 from database import DBhandler
 import sys
 
@@ -7,25 +7,70 @@ DB = DBhandler()
 
 @application.route("/") 
 def home():
-    return render_template("index.html")
-@application.route("/list") 
-def list():
-    return render_template("list.html")
+    return redirect(url_for('list_restaurants'))
+
+@application.route("/list") #리스트
+def list_restaurants():
+    page = request.args.get("page", 0, type=int)
+    limit = 10
+    start_idx=limit*page
+    end_idx=limit*(page+1)
+    data = DB.get_restaurants() #read the table
+    tot_count = len(data)
+    data = dict(list(data.items())[start_idx:end_idx])
+    return render_template(
+        "list.html",
+        datas=data.items(),
+        total=tot_count,
+        limit=limit,
+        page=page,
+        page_count=int((tot_count/10)+1))
+
+@application.route('/dynamicurl/<varible_name>/') #다이내믹
+def DynamicUrl(varible_name):
+    return str(varible_name)
+
+@application.route("/view_detail/<name>/") #디테일
+def view_restaurant_detail(name):
+    data = DB.get_restaurant_byname(str(name))
+    avg_rate = DB.get_avgrate_byname(str(name))
+    print("####data:",data)
+    return render_template("detail.html", data=data, avg_rate=avg_rate)
+
+@application.route("/list_foods/<res_name>/") # 식당 이름 기반 동적 라우팅
+def view_foods(res_name):
+    data = DB.get_food_byname(str(res_name))
+    tot_count = len(data)
+    page_count = len(data)
+    return render_template(
+        "food_list.html",
+        datas=data,
+        total=tot_count)
+
 @application.route("/register_restaurant") 
 def register_restaurant():
     return render_template("register_restaurant.html")
+
 @application.route("/register_menu") 
 def register_menu():
     return render_template("register_menu.html")
-@application.route("/register_review") 
-def register_review():
-    return render_template("register_review.html")
 
 @application.route("/register_menu", methods=['POST']) 
 def reg_menu():
     data=request.form 
     print(data)
     return render_template("register_menu.html", data=data)
+
+@application.route("/register_review") 
+def register_review():
+    return render_template("register_review.html")
+
+@application.route("/register_review", methods=['POST']) 
+def reg_review():
+    data=request.form 
+    return render_template("register_review.html", data=data)
+
+
 
 @application.route("/submit_restaurant_post", methods=['POST']) 
 def submit_restaurant_post():
@@ -63,7 +108,7 @@ def submit_menu_post():
 def submit_review_post():
     data=request.form
     print(data)
-    DB.insert_review(data['text'], data)
+    DB.insert_review(data['res_name'], data)
 
     return render_template("index.html", data=data)
 
--- database.py
+++ database.py
@@ -8,16 +8,24 @@ class DBhandler:
 
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
+
+    def get_restaurants(self):
+        restaurants = self.db.child("restaurant").get().val()
+        return(restaurants)
     
     def restaurant_duplicate_check(self, name): 
-        restaurants = self.db.child("restaurant").get() 
-        for res in restaurants.each():
-            if res.key() == name: 
-                return False
-            return True
+        restaurants = self.db.child("restaurant").get()
+        print(restaurants)
+        if restaurants:
+            for res in restaurants.each():
+                value = res.val()
+                if value['name'] == name: 
+                    return False
+                return True
     
     def insert_restaurant(self, name, data, path):
         restaurant_info ={
+            "name": name,
             "location": data['location'],
             "addr": data['addr'],
             "tel": data['tel'],
@@ -29,10 +37,11 @@ class DBhandler:
             "reserve": data['reserve'],
             "img": path,
         }
+        
         if self.restaurant_duplicate_check(name): 
-            self.db.child("restaurant").child(name).set(restaurant_info) 
-            print(data, path)
-            return True
+           self.db.child("restaurant").push(restaurant_info)
+           print(data, path)
+           return True
         else:
             return False
     
@@ -48,10 +57,37 @@ class DBhandler:
 
     def insert_review(self, name, data):
         review_info = {
+            "res_name" : data['res_name'],
             "star": data['star'],
             "text": data['text'],
         }
         self.db.child("review").child(name).set(review_info) 
         return True
 
-    
+    def get_restaurant_byname(self, name):
+        restaurants = self.db.child("restaurant").get()
+        target_value=""
+        for res in restaurants.each():
+            value = res.val()
+            if value['name'] == name:
+                target_value=value
+                return target_value
+
+    def get_food_byname(self, name):
+        restaurants = self.db.child("menu").get()
+        target_value=[]
+        for res in restaurants.each():
+            value = res.val()
+            if value['res_name'] == name:
+                target_value.append(value)
+                return target_value
+
+    def get_avgrate_byname(self,name):
+        reviews = self.db.child("review").get()
+        rates=[]
+        for res in reviews.each():
+            value = res.val()
+            print('뭐야', value)
+            if value['res_name'] == name:
+                rates.append(float(value['rate']))
+                return sum(rates)/len(rates)
